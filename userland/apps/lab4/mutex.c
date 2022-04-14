3cdec4e0f3b802cdcc4bf36facd2d2e99933c48f
Naplesoul <shenwhang@outlook.com>
2022-04-14 20:42:46 +0800

    feat: add mutex using semaphore

--- userland/apps/lab4/mutex.c
+++ userland/apps/lab4/mutex.c
@@ -3,23 +3,26 @@
 #include "mutex.h"
 #include <stdio.h>
 
+int sem;
+
 void lock_init(struct lock *lock)
 {
         /* LAB 4 TODO BEGIN */
-
+        sem = __chcore_sys_create_sem();
+        __chcore_sys_signal_sem(sem);
         /* LAB 4 TODO END */
 }
 
 void lock(struct lock *lock)
 {
         /* LAB 4 TODO BEGIN */
-
+        __chcore_sys_wait_sem(sem, 1);
         /* LAB 4 TODO END */
 }
 
 void unlock(struct lock *lock)
 {
         /* LAB 4 TODO BEGIN */
-
+        __chcore_sys_signal_sem(sem);
         /* LAB 4 TODO END */
 }
